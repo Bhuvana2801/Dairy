71940b19c53742f1a95271fb3cb07fe484ceaa3d
unknown <vangabhuvana018@gmail.com>
2026-09-16 20:31:49 +0530

    Fix Invoice End Date calculation logic referencing flatpickr

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ayyappa-dairy-v12'; // Updated version to force refresh
+const CACHE_NAME = 'ayyappa-dairy-v13'; // Updated version to force refresh
 const ASSETS = [
   './',
   './index.html',
