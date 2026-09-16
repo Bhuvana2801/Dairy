08cdde07175db6c4e4c059fb16397acca3ba69a0
unknown <vangabhuvana018@gmail.com>
2026-09-16 20:38:35 +0530

    Implement Admin CID assignment for 15 Days Plan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ayyappa-dairy-v13'; // Updated version to force refresh
+const CACHE_NAME = 'ayyappa-dairy-v14'; // Updated version to force refresh
 const ASSETS = [
   './',
   './index.html',
